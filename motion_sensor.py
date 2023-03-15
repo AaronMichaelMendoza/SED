b457dbda76e19fd5629d0f5fc123c64995a6396f
Aaron Mendoza <aaron_mendoza1@baylor.edu>
2023-03-15 14:25:35 -0500

    Update motion_sensor.py
    fixed detect_motion

--- motion_sensor.py
+++ motion_sensor.py
@@ -5,18 +5,19 @@
 # Last Modified: 3/15/2023
 
 import sensor, image, time, pyb
+from pyb import Pin
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
-motion_pin = pyb.Pin("P0", pyb.Pin.IN, pyb.Pin.PULL_DOWN)
+motion_pin = Pin("P0", Pin.IN, Pin.PULL_DOWN)
 
 clock = time.clock()
 
 def detect_motion():
-    if (motion_pin == 1):
+    if (motion_pin.value() == 1):
         print('MOTION DETECTED')
 
 while(True):
