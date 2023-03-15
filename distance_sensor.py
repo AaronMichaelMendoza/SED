0de35a4ef984c772c58626465654523ad25dabd9
Aaron Mendoza <aaron_mendoza1@baylor.edu>
2023-03-15 14:13:37 -0500

    Distance and Motion
    - motion sensor test added
    - added I2C library and example to distance_sensor.py

--- distance_sensor.py
+++ distance_sensor.py
@@ -2,14 +2,22 @@
 # Description:   This script holds the functions used to interface with the distance sensor.
 # Authors:       C. Jackson, J. Markle, C. McCarver, A. Mendoza, A. White, H. Williams
 # Date Created:  3/3/2023
-# Last Modified: 3/3/2023
+# Last Modified: 3/15/2023
 
-import sensor, image, time
+import sensor, image, time, pyb
+from pyb import I2C
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
+
+i2c = I2C(2, I2C.MASTER, baudrate=100000)
+i2c.scan() # returns list of slave addresses
+i2c.send('hello', 0x42) # send 5 bytes to slave with address 0x42
+i2c.recv(5, 0x42) # receive 5 bytes from slave
+i2c.mem_read(2, 0x42, 0x10) # read 2 bytes from slave 0x42, slave memory 0x10
+i2c.mem_write('xy', 0x42, 0x10) # write 2 bytes to slave 0x42, slave memory 0x10
 
 clock = time.clock()
 
--- motion_sensor.py
+++ motion_sensor.py
@@ -2,18 +2,22 @@
 # Description:   This script holds the functions used to interface with the motion sensor.
 # Authors:       C. Jackson, J. Markle, C. McCarver, A. Mendoza, A. White, H. Williams
 # Date Created:  3/3/2023
-# Last Modified: 3/3/2023
+# Last Modified: 3/15/2023
 
-import sensor, image, time
+import sensor, image, time, pyb
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
+motion_pin = pyb.Pin("P0", pyb.Pin.IN, pyb.Pin.PULL_DOWN)
+
 clock = time.clock()
 
+def detect_motion():
+    if (motion_pin == 1):
+        print('MOTION DETECTED')
+
 while(True):
-    clock.tick()
-    img = sensor.snapshot()
-    print(clock.fps())
+    detect_motion()
