dcb0b0655655023206917a6cff4fb9de536b7cdc
Aaron Mendoza <aaron_mendoza1@baylor.edu>
2023-03-15 14:59:17 -0500

    Update distance_sensor.py
    scuffed version of distance_sensor

--- distance_sensor.py
+++ distance_sensor.py
@@ -12,16 +12,19 @@ sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
-i2c = I2C(2, I2C.MASTER, baudrate=100000)
+i2c = I2C(2) #initialize I2C bus 2
 i2c.scan() # returns list of slave addresses
-i2c.send('hello', 0x42) # send 5 bytes to slave with address 0x42
-i2c.recv(5, 0x42) # receive 5 bytes from slave
-i2c.mem_read(2, 0x42, 0x10) # read 2 bytes from slave 0x42, slave memory 0x10
-i2c.mem_write('xy', 0x42, 0x10) # write 2 bytes to slave 0x42, slave memory 0x10
+
+def readDistance():
+    i2c.send(1, 0x70) # send 1 byte to slave with address 0x70
+    i2c.recv(byte, 0x70) # receive 1 byte from slave
+
+
+
+#i2c.mem_read(2, 0x42, 0x10) # read 2 bytes from slave 0x42, slave memory 0x10
+#i2c.mem_write('xy', 0x42, 0x10) # write 2 bytes to slave 0x42, slave memory 0x10
 
 clock = time.clock()
 
 while(True):
-    clock.tick()
-    img = sensor.snapshot()
-    print(clock.fps())
+   readDistance()
